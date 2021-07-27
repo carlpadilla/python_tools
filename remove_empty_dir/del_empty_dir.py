42b71bda59c1439d25e5e1b64679a99e1db344a7
carlpadilla <carlpadilla@protonmail.com>
2021-07-27 10:43:43 -0400

    first commit

---
+++ remove_empty_dir/del_empty_dir.py
@@ -0,0 +1,45 @@
+#    Usage: python3 del_empty_dir.py "E:/Test"
+
+import os
+import sys
+import shutil
+import time
+
+path = sys.argv[1]
+
+
+def move_up_dir():
+    for root, dirs, files in os.walk(path, topdown=False):
+        for file in files:
+            try:
+                print(file)
+                shutil.move(os.path.join(root, file), path)
+                os.remove(file)
+            # os.rmdir()
+            except OSError:
+                pass
+
+
+def del_empty_dir():
+    if len(sys.argv) == 1:
+        # Print usage
+        print("Usage: del_empty_dir.py \"E:/TestFolder\"")
+    else:
+        for root, dirs, files in os.walk(path, topdown=False):
+            for name in dirs:
+                try:
+                    if len(os.listdir(os.path.join(root, name))) == 0:  # check whether the directory is empty
+                        print("Deleting", os.path.join(root, name))
+                        try:
+                            os.rmdir(os.path.join(root, name))
+                        except:
+                            print("FAILED :", os.path.join(root, name))
+                            pass
+                except:
+                    pass
+
+
+move_up_dir()
+time.sleep(5)
+del_empty_dir()
+
